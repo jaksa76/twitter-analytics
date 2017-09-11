776e0a2fb0d3cb10b6bb5f25a7cad1f01356802c
sf105 <github@m3p.co.uk>
2017-09-11 15:21:28 +0200

    simplify booleans

--- src/main/java/com/zuhlke/ta/sentiment/utils/POSUtils.java
+++ src/main/java/com/zuhlke/ta/sentiment/utils/POSUtils.java
@@ -3,48 +3,34 @@ package com.zuhlke.ta.sentiment.utils;
 public class POSUtils {
 	
 	public static boolean isVerb(String word){
-		if(word.endsWith("VB") || word.endsWith("VBD") || word.endsWith("VBG")
-				|| word.endsWith("VBN") || word.endsWith("VBP") ||  word.endsWith("VBZ"))
-			return true;
-		return false;
+		return word.endsWith("VB") || word.endsWith("VBD") || word.endsWith("VBG")
+				|| word.endsWith("VBN") || word.endsWith("VBP") || word.endsWith("VBZ");
 	}
 	
 	public static boolean isNoun(String word){
-		if(word.endsWith("NN") || word.endsWith("NNS"))
-			return true;
-		return false;
+		return word.endsWith("NN") || word.endsWith("NNS");
 	}
 
 	public static boolean isAdjective(String word){
-		if(word.endsWith("JJ") || word.endsWith("JJR") || word.endsWith("JJS"))
-			return true;
-		return false;
+		return word.endsWith("JJ") || word.endsWith("JJR") || word.endsWith("JJS");
 	}
 
 	public static boolean isAdverb(String word){
-		if(word.endsWith("RB") || word.endsWith("RBR") || word.endsWith("RBS") || word.endsWith("WRB"))
-			return true;
-		return false;
+		return word.endsWith("RB") || word.endsWith("RBR") || word.endsWith("RBS") || word.endsWith("WRB");
 	}
 	
 	public static boolean isLimitWord(String word){
-		if(word.endsWith("CC") || word.endsWith("IN") || word.endsWith("WDT") || 
-					word.endsWith("WP") || word.endsWith("WP$"))
-			return true;
-		return false;
+		return word.endsWith("CC") || word.endsWith("IN") || word.endsWith("WDT") ||
+				word.endsWith("WP") || word.endsWith("WP$");
 	}
 	
 	public static boolean isSubject(String word){
-		if(word.endsWith("NN") || word.endsWith("NNS") || word.endsWith("NNP") || 
-					word.endsWith("NNPS") || word.endsWith("PRP") || word.endsWith("PRP$"))
-			return true;
-		return false;
+		return word.endsWith("NN") || word.endsWith("NNS") || word.endsWith("NNP") ||
+				word.endsWith("NNPS") || word.endsWith("PRP") || word.endsWith("PRP$");
 	}
 	
 	public static boolean isModal(String word){
-		if(word.endsWith("MD"))
-			return true;
-		return false;
+		return word.endsWith("MD");
 	}
 	
 	public static String stripWord(String word){
