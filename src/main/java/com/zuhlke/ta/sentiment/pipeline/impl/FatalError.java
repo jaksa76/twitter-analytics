1deac5ae1b09fffed23f76ae77735bdcefaf8488
sf105 <github@m3p.co.uk>
2017-09-21 16:30:43 +0100

    Replace MapDB with BigQuery

--- src/main/java/com/zuhlke/ta/sentiment/pipeline/impl/FatalError.java
+++ src/main/java/com/zuhlke/ta/sentiment/pipeline/impl/FatalError.java
@@ -1,6 +1,10 @@
 package com.zuhlke.ta.sentiment.pipeline.impl;
 
 public class FatalError extends RuntimeException {
+    public FatalError(String message) {
+        super(message);
+    }
+
     public FatalError(String message, Throwable cause) {
         super(message, cause);
     }
