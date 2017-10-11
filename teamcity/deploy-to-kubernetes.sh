d1747b0862ff72dbb600eec650f100810acee685
Matthew Richards <matthew.richards@zuehlke.com>
2017-10-11 16:43:56 +0100

    Update deployment script to check for and create/upgrade worker, master and web-app separately

--- teamcity/deploy-to-kubernetes.sh
+++ teamcity/deploy-to-kubernetes.sh
@@ -3,13 +3,29 @@ set -e
 
 version=$1
 
-if [[ -z "kubectl get deployments 2>&1 | grep master" ]];
+if [[ -z "kubectl get deployments 2>&1 | grep master-deployment" ]];
 then
+    echo Creating master deployment
     kubectl create -f ../kubernetes/cluster-master-config.yml
-    kubectl create -f ../kubernetes/cluster-worker-config.yml
-    kubectl create -f ../kuberenetes/cluster-web-app-config.yml
 else
+    echo Updating master deployment to version $version
     kubectl set image deployment/master-deployment master=eu.gcr.io/apt-sentinel-180609/ta-master:$version
-    kubectl set image deployment/master-deployment master=eu.gcr.io/apt-sentinel-180609/ta-worker:$version
-    kubectl set image deployment/master-deployment master=eu.gcr.io/apt-sentinel-180609/ta-web-app:$version
+fi
+
+if [[ -z "kubectl get deployments 2>&1 | grep worker-deployment" ]];
+then
+    echo Creating worker deployment
+    kubectl create -f ../kubernetes/cluster-worker-config.yml
+else
+    echo Updating worker deployment to version $version
+    kubectl set image deployment/worker-deployment worker=eu.gcr.io/apt-sentinel-180609/ta-worker:$version
+fi
+
+if [[ -z "kubectl get deployments 2>&1 | grep web-app-deployment" ]];
+then
+    echo Creating worker deployment
+    kubectl create -f ../kubernetes/cluster-web-app-config.yml
+else
+    echo Updating worker deployment to version $version
+    kubectl set image deployment/web-app-deployment web-app=eu.gcr.io/apt-sentinel-180609/ta-web-app:$version
 fi
