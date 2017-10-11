d653997a1ddcae0620d8212abed9c8d3f34a6c1e
Matthew Richards <matthew.richards@zuehlke.com>
2017-10-11 17:00:37 +0100

    Fix file path to kubernetes configs

--- teamcity/deploy-to-kubernetes.sh
+++ teamcity/deploy-to-kubernetes.sh
@@ -6,7 +6,7 @@ version=$1
 if [[ -z `kubectl get deployments 2>&1 | grep master-deployment` ]];
 then
     echo Creating master deployment
-    kubectl create -f ../kubernetes/cluster-master-config.yml
+    kubectl create -f ./kubernetes/cluster-master-config.yml
 else
     echo Updating master deployment to version $version
     kubectl set image deployment/master-deployment master=eu.gcr.io/apt-sentinel-180609/ta-master:$version
@@ -15,7 +15,7 @@ fi
 if [[ -z `kubectl get deployments 2>&1 | grep worker-deployment` ]];
 then
     echo Creating worker deployment
-    kubectl create -f ../kubernetes/cluster-worker-config.yml
+    kubectl create -f ./kubernetes/cluster-worker-config.yml
 else
     echo Updating worker deployment to version $version
     kubectl set image deployment/worker-deployment worker=eu.gcr.io/apt-sentinel-180609/ta-worker:$version
@@ -24,7 +24,7 @@ fi
 if [[ -z `kubectl get deployments 2>&1 | grep web-app-deployment` ]];
 then
     echo Creating worker deployment
-    kubectl create -f ../kubernetes/cluster-web-app-config.yml
+    kubectl create -f ./kubernetes/cluster-web-app-config.yml
 else
     echo Updating worker deployment to version $version
     kubectl set image deployment/web-app-deployment web-app=eu.gcr.io/apt-sentinel-180609/ta-web-app:$version
