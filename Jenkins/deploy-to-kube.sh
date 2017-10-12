0c81757fddd9bf2ee67de8453f6c0deed287b5c6
Hudson, Philip <Philip.Hudson@zuhlke.com>
2017-10-12 09:21:58 +0200

    (config): test kube script kubectl location

--- Jenkins/deploy-to-kube.sh
+++ Jenkins/deploy-to-kube.sh
@@ -3,12 +3,12 @@ set -e
 
 version=$1
 
-if [[ -z `kubectl get deployments 2>&1 | grep worker-deployment` ]];
+if [[ -z `/usr/share/google-cloud-sdk/bin/kubectl get deployments 2>&1 | grep worker-deployment` ]];
 then
     echo Creating worker deployment
     sed -i "s/%VERSION%/$version/" ./worker-deployment.yaml
-    kubectl create -f ./worker-deployment.yaml
+    /usr/share/google-cloud-sdk/bin/kubectl create -f ./worker-deployment.yaml
 else
     echo Updating worker deployment to version $version
-    kubectl set image deployment/worker-deployment worker=eu.gcr.io/genuine-axe-182507/worker:$version
+    /usr/share/google-cloud-sdk/bin/kubectl set image deployment/worker-deployment worker=eu.gcr.io/genuine-axe-182507/worker:$version
 fi
