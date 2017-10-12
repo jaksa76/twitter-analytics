42cb50f154975be8e330001833dbbbf50e05fcc0
Hudson, Philip <Philip.Hudson@zuhlke.com>
2017-10-12 09:29:05 +0200

    (config): test kube script master-service

--- Jenkins/deploy-to-kube.sh
+++ Jenkins/deploy-to-kube.sh
@@ -12,3 +12,14 @@ else
     echo Updating worker deployment to version $version
     /usr/share/google-cloud-sdk/bin/kubectl set image deployment/worker-deployment worker=eu.gcr.io/genuine-axe-182507/worker:$version
 fi
+
+
+if [[ -z `/usr/share/google-cloud-sdk/bin/kubectl get deployments 2>&1 | grep master-deployment` ]];
+then
+    echo Creating master deployment
+    sed -i "s/%VERSION%/$version/" ./master-deployment.yaml
+    /usr/share/google-cloud-sdk/bin/kubectl create -f ./master-deployment.yaml
+else
+    echo Updating master deployment to version $version
+    /usr/share/google-cloud-sdk/bin/kubectl set image deployment/master-deployment master-service=eu.gcr.io/genuine-axe-182507/master:$version
+fi
